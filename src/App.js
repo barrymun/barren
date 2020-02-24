b103592c6fe5dcc23680f9c9d3d1c951840bdf48
Neil <barrymun@tcd.ie>
2020-02-24 12:28:12 +0000

    [wip] working move (but distances are incorrect)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Base} from "./_components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Base {
+
+  container = null;
+  player = null;
+
+  constructor(props) {
+    super(props);
+    this.container = React.createRef();
+    this.player = React.createRef();
+    this.mouseUp = this.mouseUp.bind(this);
+    console.log(this.container.scrollLeft, this.container.scrollTop)
+  }
+
+  mouseUp(e) {
+    const {innerWidth, innerHeight} = window;
+    const {clientX, clientY} = e;
+
+    let halfInnerWidth = ~~(innerWidth / 2);
+    let halfInnerHeight = ~~(innerHeight / 2);
+
+    console.log({halfInnerWidth, halfInnerHeight, clientX, clientY})
+
+    if (clientX >= halfInnerWidth) {
+      console.log('A')
+      this.container.scrollLeft += clientX;
+    } else if (clientX < halfInnerWidth) {
+      console.log('B')
+      this.container.scrollLeft -= clientX;
+    }
+
+    if (clientY >= halfInnerHeight) {
+      console.log('C')
+      this.container.scrollTop += clientY;
+    } else if (clientY < halfInnerHeight) {
+      console.log('D')
+      this.container.scrollTop -= clientY;
+    }
+
+  }
+
+  componentDidMount() {
+    this.container.addEventListener('mouseup', this.mouseUp);
+  }
+
+  componentWillUnmount() {
+    this.container.removeEventListener('mouseup', this.mouseUp);
+  }
+
+  render() {
+    return (<div ref={node => this.container = node} className={`container`}>
+      <div ref={node => this.player = node} className={`player`}/>
+      <div className={`map`}/>
+    </div>);
+  }
 }
 
-export default App;
+export {App};
